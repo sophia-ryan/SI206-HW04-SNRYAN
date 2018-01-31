99d13b009c2e1dda558416d8f30c333ae59c3ae6
Carol Castro <Leena@0587356883.wireless.umich.net>
2018-01-31 10:42:27 -0500

    positive

--- magic_eight.py
+++ magic_eight.py
@@ -7,3 +7,9 @@ def userInput():
  list_of_questions = ["Reply hazy try again","Ask again later", "Better not tell you now",
  "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no",
  "My sources say no", "Outlook not so good", "Very doubtful"]
+
+
+positive = ['It is certain', 'It is decidedly so',
+'Without a doubt', 'yes definitly', 'You may rely on it',
+'as i see it, yes', 'Most likey', 'Outlook good', "yes",
+"signs point to yes"]
