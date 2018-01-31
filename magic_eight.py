e65ad2328a446f9acfb1609637763ddd47ad2145
Sophia Ryan <sophiaryan@0587383158.wireless.umich.net>
2018-01-31 10:28:39 -0500

    Added check_questions

--- magic_eight.py
+++ magic_eight.py
@@ -1,9 +1,25 @@
 # Homework Four
-# NAMES
-# UNIQUE NAMES
+# Sophia Ryan, Lauren Zielinski #NAMES
+# snryan, laurzie, #UNIQUE NAMES
+
+import random
 
 def userInput():
     answer = input("What is your question?")
- list_of_questions = ["Reply hazy try again","Ask again later", "Better not tell you now",
- "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no",
- "My sources say no", "Outlook not so good", "Very doubtful"]
+    return answer
+    
+
+list_of_questions = ["Reply hazy try again","Ask again later", "Better not tell you now",
+                     "Cannot predict now", "Concentrate and ask again", "Don't count on it",
+                     "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+
+def checkInput():
+    input = userInput()
+    suffix = "?"
+    sorry = “I’m sorry, I can only answer questions.”
+    randomAnswer = random.choice(list_of_questions)
+    while input != "quit" or "Quit":
+        if input.endswith(suffix) != true:
+            print(sorry)
+        else:
+            return randomAnswer
