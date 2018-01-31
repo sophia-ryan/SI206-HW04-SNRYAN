a47c7c356f4023f36a4831c64a5c23b545661ed4
Lauren Zielinski <laurzie@umich.edu>
2018-01-31 10:05:15 -0500

    Adding 10 Questions

--- magic_eight.py
+++ magic_eight.py
@@ -4,3 +4,6 @@
 
 def userInput():
     answer = input("What is your question?")
+ list_of_questions = ["Reply hazy try again","Ask again later", "Better not tell you now",
+ "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no",
+ "My sources say no", "Outlook not so good", "Very doubtful"]
