837059b741c670538559b981333ae4aff7bc4d99
Lauren Zielinski <laurzie@umich.edu>
2018-01-31 11:27:31 -0500

    Updates to file

--- magic_eight.py
+++ magic_eight.py
@@ -5,22 +5,28 @@
 import random
 
 def userInput():
-    answer = input("What is your question?")
+    answer = input("What is your question? ")
     return answer
 
- list_of_questions = ["Reply hazy try again","Ask again later", "Better not tell you now",
- "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no",
- "My sources say no", "Outlook not so good", "Very doubtful","It is certain", "It is decidedly so",
- "Without a doubt", "yes definitly", "You may rely on it", "as i see it, yes", "Most likey",
- "Outlook good", "yes", "signs point to yes"]
+list_of_anwsers = ["Reply hazy try again","Ask again later", "Better not tell you now",
+"Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no",
+"My sources say no", "Outlook not so good", "Very doubtful","It is certain", "It is decidedly so",
+"Without a doubt", "yes definitly", "You may rely on it", "as i see it, yes", "Most likey",
+"Outlook good", "yes", "signs point to yes"]
 
 def checkInput():
-    input = userInput()
     suffix = "?"
-    sorry = “I’m sorry, I can only answer questions.”
-    randomAnswer = random.choice(list_of_questions)
-    while input != "quit" or "Quit":
-        if input.endswith(suffix) != true:
+    sorry = "I’m sorry, I can only answer questions."
+    randomAnswer = random.choice(list_of_anwsers)
+    while True:
+        asking_question = userInput()
+        if asking_question == "quit":
+            return False
+        elif asking_question.endswith(suffix) != True:
             print(sorry)
+            return True
         else:
-            return randomAnswer
+            print(randomAnswer)
+            return True
+
+checkInput()
