bc6e53335d346918d6534ca933324fe9530facfa
Sophia Ryan <sophiaryan@Sophia-Ryans-MacBook-Pro.local>
2018-01-31 22:57:30 -0500

    read me

---
+++ readme.py
@@ -0,0 +1,17 @@
+# Homework Four
+
+# This homework consists of a magic eight ball program in which you can ask
+# questions and it will return an answer
+
+## Getting Started
+
+## No special requirements needed
+
+### Prerequisites
+
+### This program uses python package "random"
+
+#### Authors
+
+#### Sophia Ryan, Lauren Zielinski, Carol Castro
+#### snryan, laurzie, ccastroa
